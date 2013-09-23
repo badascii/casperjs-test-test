62bfbeb4116563ae8f0777f8b455bdc57542cf6c
Jordan A. Skarbek <jaskarbek@gmail.com>
2013-09-23 11:37:17 -0700

    updated user email

--- test.js
+++ test.js
@@ -3,6 +3,33 @@ var universe = require('./universe');
 // hello-test.js
 casper.test.begin("Hello, Test!", 1, function(test) {
   console.log(universe.answer()); // prints "It's 42"
-  test.assert(true);
+  console.log(universe.roffle(2, 2));
+  universe.clickie('form[action="/search"', 'casperjs');
+	// test.assert(true);
   test.done();
 });
+
+
+
+
+// casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
+//   casper.start("http://www.google.fr/", function() {
+//     test.assertTitle("HURF", "google homepage title is the one expected");
+//     test.assertExists('form[action="/search"]', "main form is found");
+//     this.fill('form[action="/search"]', {
+//       q: "casperjs"
+//     }, true);
+//   });
+
+//   casper.then(function() {
+//     test.assertTitle("casperjs - Recherche Google", "google title is ok");
+//     test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
+//     test.assertEval(function() {
+//       return __utils__.findAll("h3.r").length >= 10;
+//     }, "google search for \"casperjs\" retrieves 10 or more results");
+//   });
+
+//   casper.run(function() {
+//     test.done();
+//   });
+// });
--- universe.js
+++ universe.js
@@ -6,5 +6,19 @@ var require = patchRequire(require);
 var utils = require('utils');
 var magic = 42;
 exports.answer = function() {
-    return utils.format("it's %d", magic);
+	return utils.format("it's %d", magic);
+};
+
+exports.roffle = function(num1, num2) {
+	return num1 + num2;
+};
+
+exports.clickie = function(uri, q) {
+	casper.test.begin('Google search retrieves 10 or more results', 1, function suite(test) {
+	  casper.start("http://www.google.fr/", function() {
+	 	  test.assertExists(uri, "main form is found");
+
+			this.fill(uri, { q: q }, true);
+		});
+	});
 };
